bcf8772b5f2dba749b8fc609d20e0f69bdd7c4f8
Ismael <scjismael@gmail.com>
2026-02-16 14:12:24 +0100

    Pruebas con hibernate, cambios en el xml

---
+++ PROYECTO_VOTACIONES/src/controlador/PruebaHibernate.java
@@ -0,0 +1,149 @@
+package controlador;
+
+import java.util.List;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.query.Query;
+
+import persistencias.PorcentajesRangoedad;
+import persistencias.VotosComunidadPartido;
+
+public class PruebaHibernate {
+
+    private SessionFactory sessionFactory;
+
+    public PruebaHibernate(SessionFactory sessionFactory) {
+        this.sessionFactory = sessionFactory;
+    }
+
+    // 1️⃣ Leer todas las comunidades
+    public void leerComunidades() {
+
+        Session session = sessionFactory.openSession();
+        Transaction tx = session.beginTransaction();
+
+        List<PorcentajesRangoedad> lista =
+                session.createQuery("FROM PorcentajesRangoedad", PorcentajesRangoedad.class)
+                       .list();
+
+        for (PorcentajesRangoedad p : lista) {
+
+            System.out.println("Comunidad: " + p.getNombreComunidad());
+            System.out.println("18-25: " + p.getRango1825() + "%");
+            System.out.println("26-40: " + p.getRango2640() + "%");
+            System.out.println("41-65: " + p.getRango4165() + "%");
+            System.out.println("+66: " + p.getRangoMas66() + "%");
+            System.out.println("Total habitantes: " + p.getTotalHabitantes());
+            System.out.println("-----------------------------------");
+        }
+
+        tx.commit();
+        session.close();
+    }
+
+    // 2️⃣ Insertar fila en VOTOS_COMUNIDAD_PARTIDO
+    public void insertarVotosIniciales(String comunidad, String partido) {
+
+        Session session = sessionFactory.openSession();
+        Transaction tx = session.beginTransaction();
+
+        VotosComunidadPartido voto = new VotosComunidadPartido();
+
+        voto.setNombreComunidad(comunidad);
+        voto.setPartidoPolitico(partido);
+
+        voto.setVotos19(0);
+        voto.setVotos1017(0);
+        voto.setVotos1825(0);
+        voto.setVotos2640(0);
+        voto.setVotos4165(0);
+        voto.setVotosMas66(0);
+        voto.setTotalVotos(0);
+
+        session.save(voto);
+
+        tx.commit();
+        session.close();
+
+        System.out.println("Fila insertada correctamente.");
+    }
+
+    // 3️⃣ Recuperar un voto por clave compuesta
+    public void recuperarVoto(String comunidad, String partido) {
+
+        Session session = sessionFactory.openSession();
+        Transaction tx = session.beginTransaction();
+
+        VotosComunidadPartido voto =
+                session.get(VotosComunidadPartido.class,
+                        new Object[]{comunidad, partido});
+
+        if (voto != null) {
+            System.out.println("Registro encontrado:");
+            System.out.println("Comunidad: " + voto.getNombreComunidad());
+            System.out.println("Partido: " + voto.getPartidoPolitico());
+            System.out.println("Total votos: " + voto.getTotalVotos());
+        } else {
+            System.out.println("No se encontró el registro.");
+        }
+
+        tx.commit();
+        session.close();
+    }
+
+    // 4️⃣ Actualizar votos con HQL
+    public void actualizarVoto(String comunidad, String partido) {
+
+        Session session = sessionFactory.openSession();
+        Transaction tx = session.beginTransaction();
+
+        Query query = session.createQuery(
+                "update VotosComunidadPartido v " +
+                "set v.votos1825 = v.votos1825 + 1, " +
+                "    v.totalVotos = v.totalVotos + 1 " +
+                "where v.nombreComunidad = :comunidad " +
+                "and v.partidoPolitico = :partido"
+        );
+
+        query.setParameter("comunidad", comunidad);
+        query.setParameter("partido", partido);
+
+        int filas = query.executeUpdate();
+
+        tx.commit();
+        session.close();
+
+        System.out.println("Filas actualizadas: " + filas);
+    }
+
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		SessionFactory sessionFactory=null;
+		Configuration configuration = new Configuration();
+		configuration.configure();
+		sessionFactory = configuration.buildSessionFactory();
+		
+		PruebaHibernate prueba = new PruebaHibernate(sessionFactory);
+
+
+		prueba.leerComunidades();
+
+		prueba.insertarVotosIniciales("Andalucia", "X");
+
+		/*
+		prueba.recuperarVoto("Andalucia", "X");
+
+		prueba.actualizarVoto("Andalucia", "X");
+
+		prueba.recuperarVoto("Andalucia", "X");
+		*/
+		 sessionFactory.close(); 
+
+	}
+
+}
